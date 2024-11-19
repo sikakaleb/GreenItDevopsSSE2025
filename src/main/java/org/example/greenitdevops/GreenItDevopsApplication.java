73ae9fe2501267d1ef855d4201b7bb79e18ee35e
sikakaleb <dassou-ablam-kaleb.sika@etu.univ-cotedazur.fr>
2024-11-19 11:28:27 +0100

    td GreenItDevops mardi 19/11/2024

---
+++ src/main/java/org/example/greenitdevops/GreenItDevopsApplication.java
@@ -0,0 +1,13 @@
+package org.example.greenitdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenItDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenItDevopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/greenitdevops/GreenItDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.greenitdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenItDevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
